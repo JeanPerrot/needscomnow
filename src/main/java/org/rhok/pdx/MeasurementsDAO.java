f274bd3d73eee763f9569800488d05d21f3ddbb2
Jean Perrot <Jean.brahnperrot@gmail.com>
2011-12-27 22:30:25 -0800

    removing files that were refactored out

--- src/main/java/org/rhok/pdx/MeasurementsDAO.java
+++
@@ -1,7 +0,0 @@
-package org.rhok.pdx;
-
-public interface MeasurementsDAO {
-    Measurements getMeasurements(Location location, double range, long timestamp, int maxCount);
-    void saveMeasurements(Measurements ms);
-
-}
